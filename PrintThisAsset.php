abe545848db7c0303a4b95a2087eef92f690792c
Manop Kongoon <kongoon@hotmail.com>
2018-03-28 21:08:19 +0700

    Update PrintThisAsset.php

--- PrintThisAsset.php
+++ PrintThisAsset.php
@@ -5,7 +5,7 @@ use yii\web\AssetBundle;
 
 class PrintThisAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/printThis';
+    public $sourcePath = '@bower/printthis';
 
     public $js = [
         '//code.jquery.com/jquery-migrate-1.3.0.min.js',
